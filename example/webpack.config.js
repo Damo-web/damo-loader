ff1d581f49ccad611c4f66515335d7d056e71c6e
snowball <snowballer0705@gmail.com>
2019-01-04 00:43:10 +0800

    :tada:初始化loader

---
+++ example/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolveLoader: {
+    alias: {
+      'vue-loader': require.resolve('../lib')
+    }
+  }
+};
